7d3f7ea567e93a6c8b829ffdf1372908c38ba158
John Pwajok <johnpwajok97@hotmail.com>
2022-01-07 19:06:22 +0000

    Readme updated and variable names changed from camelCase to underscores

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-README
+# README
+
+This script uses Selenium to automate image scraping in the Chrome browser.
+
+**REQUIREMENTS:**
+Selenium: pip3 install selenium
+Chromedriver: https://chromedriver.chromium.org/
+
+_You will need to set the 'PATH' variable to the path of the Chromedriver executable after downloading it_
+
+When run, the user will be prompted to enter a search term, the script will perform a Google image search with the specified term.
+The user will also be prompted to enter the number of images they want to download.
+
+When the two entries are made, the image URL's will be retreived from the image search then all downloaded in sequence to a directory in the same location as the script.
--- webscrape.py
+++ webscrape.py
@@ -17,30 +17,30 @@ PATH = "/Users/johnpwajok/Downloads/chromedriver"
 wd = webdriver.Chrome(PATH)
 
 
-# function to find image URL's on Google, arguments: webdriver, delay (seconds), maxImages: number of images to fetch, keyWord: search term
-def getImagesFromGoogle(wd, delay, maxImages, keyWord):
+# function to find image URL's on Google, arguments: webdriver, delay (seconds), max_images: number of images to fetch, key_word: search term
+def get_images_from_google(wd, delay, max_images, key_word):
     # Continue to scroll to bottom of chrome image search page
-    def scrollDown(wd):
+    def scroll_down(wd):
         wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(delay)
 
-    # The Chrome url of the image search (based on keyword)
-    defaultGoogleImageSearchURL = "https://www.google.com/search?tbm=isch&q="
-    url = defaultGoogleImageSearchURL + keyWord
+    # The Chrome url of the image search (based on key_word)
+    default_google_image_search_URL = "https://www.google.com/search?tbm=isch&q="
+    url = default_google_image_search_URL + key_word
     wd.get(url)
 
     # List to store image URL's
-    imageUrls = set()
-    # skip if duplicate | need to increase the maxImages if duplicate found to allow script to jump to next image
-    skips = 0
+    image_URLS = set()
+    # hops if duplicate | need to increase the max_images if duplicate found to allow script to go to next image
+    hops = 0
 
-    while len(imageUrls) + skips < maxImages:
+    while len(image_URLS) + hops < max_images:
         print("\nFetching image URL's...\n")
-        scrollDown(wd)
+        scroll_down(wd)
         # classname for thumbnails in Google image search
         thumbnails = wd.find_elements(By.CLASS_NAME, "Q4LuWd")
 
-        for img in thumbnails[len(imageUrls) + skips:maxImages]:
+        for img in thumbnails[len(image_URLS) + hops:max_images]:
             try:
                 img.click()
                 time.sleep(delay)
@@ -53,50 +53,51 @@ def getImagesFromGoogle(wd, delay, maxImages, keyWord):
                 # if the image is valid | has a source tag and has http contained in the source tag
                 if image.get_attribute('src') and 'http' in image.get_attribute('src'):
                     # skip to next image if the current image is already in the list
-                    if image.get_attribute('src') in imageUrls:
-                        maxImages += 1
-                        skips += 1
+                    if image.get_attribute('src') in image_URLS:
+                        max_images += 1
+                        hops += 1
                         break
                     else:
                         # Add the image to the list
-                        imageUrls.add(image.get_attribute('src'))
-                        print(f"Found {len(imageUrls)}")
+                        image_URLS.add(image.get_attribute('src'))
+                        print(f"Found {len(image_URLS)}")
     # return the list of image URL's
-    return imageUrls
+    return image_URLS
 
 
 # Function to download images
-def downloadImage(downloadPath, url, fileName):
+def download_image(download_path, url, file_name):
     try:
-        filePath = downloadPath + fileName
+        file_path = download_path + file_name
         #check if the download directory exists / create if it doesn't:
-        if not os.path.exists(os.path.dirname(downloadPath)):
+        if not os.path.exists(os.path.dirname(download_path)):
             try:
-                os.makedirs(os.path.dirname(downloadPath))
+                os.makedirs(os.path.dirname(download_path))
                 
             except Exception as e:
                 print("The download directory cannot be created!  - ", e)
 
         #save the image
-        with open(filePath, "wb") as f:
+        with open(file_path, "wb") as f:
             try:
-                imageContent = requests.get(url, stream=True).content
-                imageFile = io.BytesIO(imageContent)
-                image = Image.open(imageFile)
+                image_content = requests.get(url, stream=True).content
+                image_file = io.BytesIO(image_content)
+                image = Image.open(image_file)
                 #Save the image
                 image.save(f, "JPEG")
                 print("Download success...")
             except Exception as e:
+                #if the image is RGBA or PNG, need to convert it to RGB to be able to save as jpg
                 s = str(e)
-                rgbString = "RGBA"
-                pngString = "cannot write mode P as JPEG"
-                if rgbString or pngString in s:
-                    with open(filePath, "wb") as f:
-                        imageContent = requests.get(url, stream=True).content
-                        imageFile = io.BytesIO(imageContent)
-                        image = Image.open(imageFile)
+                rgb_string = "RGBA"
+                png_string = "cannot write mode P as JPEG"
+                if rgb_string or png_string in s:
+                    with open(file_path, "wb") as f:
+                        image_content = requests.get(url, stream=True).content
+                        image_file = io.BytesIO(image_content)
+                        image = Image.open(image_file)
                         print("\nConverting RGBA image...\n")
-                        
+                        #convert to 'RGB'
                         image = image.convert('RGB')
                         image.save(f, "JPEG")
                         print("\nRGBA Image Conversion complete!\n")
@@ -112,13 +113,15 @@ def downloadImage(downloadPath, url, fileName):
         print("Download Failed!!  - ", e)
 
 
-#downloadImage("", imageURL, "test.jpg")
-searchTerm = input("What images would you like to download? \n")
-downloadLimit = int(input("\nHow many images do you want to download? \n"))
-urls = getImagesFromGoogle(wd, 0.5, downloadLimit, searchTerm)
+#download_image("", imageURL, "test.jpg")
+search_term = input("What images would you like to download? \n")
+download_limit = int(input("\nHow many images do you want to download? \n"))
+#get list of image url's 
+urls = get_images_from_google(wd, 0.5, download_limit, search_term)
 print("\nBeginning download...")
+#download the images from the urls list to a new directory called 'imageFolder'
 for i, url in enumerate(urls):
-    downloadImage(
+    download_image(
         "imageFolder/", url, str(i) + ".jpg")
 print("\nDownload complete!")
 
